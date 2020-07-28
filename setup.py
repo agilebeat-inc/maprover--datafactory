1308c6625063c9b1731aa13719f2106459a0b606
Marek Dwulit <mdwulit@Mareks-MBP-2.fios-router.home>
2020-07-27 20:49:39 -0400

    WIP: broken

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pointfactory',
+setup(name='datafactory',
       version='0.1.0',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      description='The data generator',
+      url='https://github.com/agilebeat-inc/maprover--datafactory',
+      author='Marek Dwulit',
+      author_email='office@agilebeat.com',
       license='MIT',
-      packages=['funniest'],
+      packages=['datafactory'],
       zip_safe=False)
---
+++ tests/covid19_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from generators import covid19 as c19
+
+class TestCovid19(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
